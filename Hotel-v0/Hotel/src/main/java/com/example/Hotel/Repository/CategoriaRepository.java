38a5de3dc036a01f0f2ee0c00dfea6c9ec329ec3
RQAP <rafaelqap410@gmail.com>
2026-06-06 14:23:41 -0300

    Delete Hotel-v0/Hotel directory

--- Hotel-v0/Hotel/src/main/java/com/example/Hotel/Repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Hotel.Repository;
-
-import com.example.Hotel.Model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
